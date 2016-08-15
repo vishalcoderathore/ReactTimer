bf7c11d18c70bb69d7bff7949a461c9048606bb3
Vishal <vshl.rathore1@gmail.com>
2016-08-15 16:05:58 -0400

    Added Navigation

--- app/app.jsx
+++ app/app.jsx
@@ -2,7 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
-
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 
 //Load Foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
@@ -14,6 +15,8 @@ require('style!css!sass!applicationStyles');
 ReactDOM.render(
     <Router history = {hashHistory}>
       <Route path = "/" component = {Main}>
+        <IndexRoute component = {Timer}></IndexRoute>
+        <Route path = "countdown" component = {Countdown}></Route>
       </Route>
     </Router>,
     document.getElementById('app'));
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -3,17 +3,16 @@ var Nav = require('Nav');
 /*Arrow function
 */
 var Main = (props) => {
-  return(
-    <div>
-      <div>
+    return (
         <div>
-          <Nav></Nav>
-          <p>Main.jsx Rendered</p>
-          {props.children}
+            <Nav/>
+            <div className="row">
+                <div className="columns medium-6 large-4 small-centered">
+                    {props.children}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 module.exports = Main;
