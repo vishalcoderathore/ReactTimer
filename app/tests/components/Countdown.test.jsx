804d1dc082866579153d1c73af97dcdbe11dd505
Vishal <vshl.rathore1@gmail.com>
2016-08-15 22:55:32 -0400

    Added Countdown Functionality with Test Cases

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,41 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', () => {
+  it('Countdown exists', () => {
+    expect(Countdown).toExist();
+  });
+
+  describe('handleSetCountdown', () => {
+    it('should set state to started and countdown', () => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(10);
+
+      expect(countdown.state.count).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+    });
+
+    it('should decrease count after 1 sec', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(10);
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(9);
+        done();
+      }, 1001);
+    });
+
+    it('should not decrease count beyond 0', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(2);
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        done();
+      }, 4000);
+    });
+  });
+});
